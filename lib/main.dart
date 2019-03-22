d8e486b3690d5a8e9de0c105ecc7ec5b3b4b5c6b
Martin Bianchi <mbianchi@mstech.la>
2019-03-22 14:05:45 -0300

    finished

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child:FlareActor(
+          "assets/solar.flr",
+          fit:BoxFit.cover,
+          alignment:Alignment.center,
+          animation: 'rotate',
+        ),
+      ),
+    );
+  }
+  
+}
